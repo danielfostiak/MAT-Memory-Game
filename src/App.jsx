01f906877a47b64628db0c1744e130d0d28f9137
Daniel <fostiakd@gmail.com>
2023-08-20 15:59:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Cards from "./components/Cards";
+
+function App() {
+  return (
+    <div>
+      <h1>Cardflip</h1>
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cards.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import Card from "./Card";
+
+function Cards() {
+  const [items, setItems] = useState(
+    [
+      { id: 1, img: "/img/html.png", stat: "" },
+      { id: 1, img: "/img/html.png", stat: "" },
+      { id: 2, img: "/img/css.png", stat: "" },
+      { id: 2, img: "/img/css.png", stat: "" },
+      { id: 3, img: "/img/js.png", stat: "" },
+      { id: 3, img: "/img/js.png", stat: "" },
+      { id: 4, img: "/img/scss.png", stat: "" },
+      { id: 4, img: "/img/scss.png", stat: "" },
+      { id: 5, img: "/img/react.png", stat: "" },
+      { id: 5, img: "/img/react.png", stat: "" },
+      { id: 6, img: "/img/vue.png", stat: "" },
+      { id: 6, img: "/img/vue.png", stat: "" },
+      { id: 7, img: "/img/angular.png", stat: "" },
+      { id: 7, img: "/img/angular.png", stat: "" },
+      { id: 8, img: "/img/nodejs.png", stat: "" },
+      { id: 8, img: "/img/nodejs.png", stat: "" },
+    ].sort(() => Math.random() - 0.5)
+  );
+
+  const [prev, setPrev] = useState(-1);
+  const [locked, setLocked] = useState(false);
+
+  function check(current) {
+    if (locked) return;
+    if (items[current].id == items[prev].id) {
+      items[current].stat = "correct";
+      items[prev].stat = "correct";
+      setItems([...items]);
+      setPrev(-1);
+      setLocked(false);
+    } else {
+      items[current].stat = "wrong";
+      items[prev].stat = "wrong";
+      setItems([...items]);
+      setTimeout(() => {
+        items[current].stat = "";
+        items[prev].stat = "";
+        setItems([...items]);
+        setPrev(-1);
+        setLocked(false);
+      }, 1000);
+    }
+  }
+
+  function handleClick(id) {
+    if (locked) return;
+    if (prev === -1) {
+      items[id].stat = "active";
+      setItems([...items]);
+      setPrev(id);
+    } else {
+      if (id == prev) return;
+      setLocked(true);
+      check(id);
+    }
+  }
+
+  return (
+    <div className="container">
+      {items.map((item, index) => (
+        <Card key={index} item={item} id={index} handleClick={handleClick} />
+      ))}
+    </div>
+  );
+}
+
+export default Cards;
