5d2fd693bc289f8d7f3a4569026bbecb0d6c9b3f
Daniel <fostiakd@gmail.com>
2023-08-25 02:50:18 +0100

    status update

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Analytics } from "@vercel/analytics/react";
+// Only testing out for now ....
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
